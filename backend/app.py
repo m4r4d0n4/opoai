79c8c16566def4c33f49f71ec28102db2b999b39
laboratorio <laboratorio>
2025-03-29 12:28:32 +0000

    ssl

--- backend/app.py
+++ backend/app.py
@@ -98,4 +98,4 @@ def proxy():
         return jsonify({"error": "No se pudo conectar al servidor privado"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=("/ssl/opoai.es_ssl_certificate.cer", "/ssl/opoai.key"))
